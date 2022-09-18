cc7e3e876a68de0270caded375c49522ca99d0f8
Harper Hsiao <72118443+Harperxx25@users.noreply.github.com>
2022-09-18 17:23:29 +1000

    Final Version ToDo List

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    /* 1. have a folder call 'views', and inside the folder had a 'list.ejs'  */
+    listTitle: day,
+    /* 2. passing a single veriable from list.ejs call listTitle, the value is the day */
+    newListItems: items
+  });
+});
+
+app.post("/", function(req, res) {
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work"); /* trigger app.get("/work") */
+  } else {
+    items.push(item);
+    res.redirect("/"); /* trigger app.get("/") */
+  }
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
